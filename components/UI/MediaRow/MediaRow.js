ded633651686027b10b9c0b9c0f8a71dd8068210
Jermaine henry <jerbunhenry@gmail.com>
2021-08-07 15:00:07 -0400

    Changed the images hope they work and now the differen genres actually show different pics with api calling

--- components/UI/MediaRow/MediaRow.js
+++ components/UI/MediaRow/MediaRow.js
@@ -1,59 +1,78 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
-
+import axios from "axios";
+import { shuffleArray } from "../../Utilities";
 
 const MediaRow = (props) => {
-    const [loadingData, setLoadingData] = useState(true);
+  const [loadingData, setLoadingData] = useState(true);
+  const [movies, setMoviesData] = useState([]);
 
-    const loopComp = (comp, digit) => {
-        let thumbnails = [];
-        for (let index = 0; index <= digit; index++) {
-            thumbnails.push(comp);
-        }
-        return thumbnails;
-    }
-    //Should Simulate loading up an image for the thumbnails 
-    const showThumbNails = () => {
-        setTimeout(() => setLoadingData(false), 400);
-        return loadingData ? loopComp(<Skeleton />, 10) :
-                            loopComp(<Thumbnail />, 10)
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/${props.endpoint}&api_key=1cf7f7e617b87f5547cd6011c423719d&language=en-US`
+      )
+      .then((res) => {
+        setMoviesData(shuffleArray(res.data.results));
+        //Handles successful response
+        setLoadingData(false);
+        console.log("good response for " + props.title);
+      })
+      .catch((err) => {
+        //Handles an error
+        console.log("error response for " + props.title);
+      });
+  }, []);
 
+  const loopComp = (comp, digit) => {
+    let thumbnails = [];
+    for (let index = 0; index <= digit; index++) {
+      thumbnails.push(comp);
     }
-    return (
-        <div className={`media-row ${props.type}`}>
-            <h3 className="media-row_title">{props.title}</h3>
-            <div className="media-row_thumbnails">
-                {showThumbNails()}
-                {/* {loopComp(
+    return thumbnails;
+  };
+  //Should Simulate loading up an image for the thumbnails
+  const showThumbNails = () => {
+    return loadingData
+      ? loopComp(<Skeleton />, 10)
+      : movies.map((movie) => {
+          return <Thumbnail movieData={movie} />;
+        });
+  };
+  return (
+    <div className={`media-row ${props.type}`}>
+      <h3 className="media-row_title">{props.title}</h3>
+      <div className="media-row_thumbnails">
+        {showThumbNails()}
+        {/* {loopComp(
                     (<Thumbnail />), 10
 
                 )} */}
+      </div>
+    </div>
+  );
+};
 
-
-
-
-            </div>
-
-        </div>
-    )
-}
-
-const Thumbnail = () => {
-    return (<div className="media-row_thumbnail">
-        <Image alt="BlackPanther Movie Poster" src={require("/public/img/BadBatch.jpg").default} objectFit='cover' width={400} height={600} />
-        <div className="media-row_top-layer">
-            <i className="fa fa-play" />
-
-        </div>
-
-    </div>)
-}
+const Thumbnail = (props) => {
+  return (
+    <div className="media-row_thumbnail">
+      <img
+        alt="Star Wars Bad Batch"
+        src={`https://image.tmdb.org/t/p/original${props.movieData.poster_path}`}
+        layout="fill"
+      />
+      <div className="media-row_top-layer">
+        <i className="fa fa-play" />
+      </div>
+    </div>
+  );
+};
 
 const Skeleton = () => {
-    return (<div className="media-row_thumbnail-skeleton">
-        <div className="media-row_thumbnail-skeleton-img">
-
-        </div>
-    </div>)
-}
+  return (
+    <div className="media-row_thumbnail-skeleton">
+      <div className="media-row_thumbnail-skeleton-img"></div>
+    </div>
+  );
+};
 export default MediaRow;
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,57 @@
-import Head from 'next/head'
-import { useEffect } from 'react'
-import { useStateContext } from '../components/UI/HBOProvider'
-import { useRouter } from 'next/router'
-import Login from '../components/UI/Login/Login'
-import MainLayout from '../components/layouts/MainLayout'
-import Image from 'next/image'
-import Header from '../components/UI/Header/Header'
-import FeaturedMedia from '../components/UI/FeaturedMedia/FeaturedMedia'
+import Head from "next/head";
+import { useEffect } from "react";
+import { useStateContext } from "../components/UI/HBOProvider";
+import { useRouter } from "next/router";
+import Login from "../components/UI/Login/Login";
+import MainLayout from "../components/layouts/MainLayout";
+import Image from "next/image";
+import Header from "../components/UI/Header/Header";
+import FeaturedMedia from "../components/UI/FeaturedMedia/FeaturedMedia";
 
-import AuthCheck from '../components/UI/AuthCheck'
-import MediaRow from '../components/UI/MediaRow/MediaRow'
+import AuthCheck from "../components/UI/AuthCheck";
+import MediaRow from "../components/UI/MediaRow/MediaRow";
 export default function Home() {
   const globalState = useStateContext();
   const router = useRouter();
-  useEffect(()=>{
- 
-  }, [])
+  useEffect(() => {}, []);
   return AuthCheck(
     <MainLayout>
       <FeaturedMedia />
-      <MediaRow title="Movies" type="large-v"/>
-      <MediaRow title="Series" type="small-h"/>
-      <MediaRow title="Action" type="small-v"/>
-      <MediaRow title="Adventure" type="small-h"/>
-      <MediaRow title="Horror" type="small-v"/>
-
-
-     
+      <MediaRow
+        title="Movies"
+        type="large-v"
+        endpoint="discover/movie?sort_by=popularity.desc&primary_release_year=2021"
+      />
+           <MediaRow
+        title="Series"
+        type="large-v"
+        endpoint="discover/tv?sort_by=popularity.desc&primary_release_year=2021"
+      />
+      <MediaRow
+        title="Action"
+        type="small-v"
+        endpoint="discover/movie?with_genres=28&primary_release_year=2021"
+      />
+       <MediaRow
+        title="Adventure"
+        type="small-h"
+        endpoint="discover/movie?with_genres=12&primary_release_year=2021"
+      />
+      <MediaRow
+        title="Animation"
+        type="small-h"
+        endpoint="discover/movie?with_genres=16&primary_release_year=2021"
+      />
+      <MediaRow
+        title="Horror"
+        type="large-v"
+        endpoint="discover/movie?with_genres=27&primary_release_year=2021"
+      />
+        <MediaRow
+        title="Comedy"
+        type="large-v"
+        endpoint="discover/movie?with_genres=35&primary_release_year=2021"
+      />
     </MainLayout>
-  )
+  );
 }
