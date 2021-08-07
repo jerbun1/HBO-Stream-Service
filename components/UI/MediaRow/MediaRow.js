af671a5c63fd5a53a3f2d31d19e1d4f85f0e9444
Jermaine henry <jerbunhenry@gmail.com>
2021-08-07 15:16:12 -0400

    second try Changed the images hope they work and now the differen genres actually show different pics with api calling

--- components/UI/MediaRow/MediaRow.js
+++ components/UI/MediaRow/MediaRow.js
@@ -56,7 +56,7 @@ const MediaRow = (props) => {
 const Thumbnail = (props) => {
   return (
     <div className="media-row_thumbnail">
-      <img
+      <Image
         alt="Star Wars Bad Batch"
         src={`https://image.tmdb.org/t/p/original${props.movieData.poster_path}`}
         layout="fill"
